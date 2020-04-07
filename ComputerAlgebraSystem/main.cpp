c5d7ee1da9d9401796e91ce81a174f9cb90c5814
rodion <rad89289317404@mail.ru>
2020-04-07 21:01:37 +0400

    system implementation

---
+++ ComputerAlgebraSystem/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "natural.h"
+#include "natural.cpp"
+#include "integer.h"
+#include "integer.cpp"
+#include "rational.h"
+#include "rational.cpp"
+#include "polynomial.h"
+#include "polynomial.cpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ ComputerAlgebraSystem/natural.cpp
@@ -0,0 +1,1 @@
+#include "natural.h"
