01a0862b0bcaf25e37543ef1fe29504d1cf89381
rodion <rad89289317404@mail.ru>
2020-04-08 20:43:25 +0400

    system implementation

--- ComputerAlgebraSystem/natural.cpp
+++ ComputerAlgebraSystem/natural.cpp
@@ -1,1 +1,65 @@
 #include "natural.h"
+
+natural::natural(const std::string &str):
+    quantity_(str.size()), digits_(new int [quantity_])
+{
+    for(int i = 0; i < quantity_; ++i)
+    {
+        digits_[quantity_ - 1 - i] = (str[i] - '0');
+    }
+}
+
+natural::natural() :
+    quantity_(1), digits_(new int)
+{
+    digits_[0] = 0;
+}
+
+natural::natural(const natural &other):
+    quantity_(other.quantity_), digits_(new int [quantity_])
+{
+    for(int i = 0; i < quantity_; ++i)
+    {
+        digits_[i] = other.digits_[i];
+    }
+}
+
+natural::~natural()
+{
+    delete[] digits_;
+}
+
+natural & natural::operator=(const natural &other)
+{
+    if(digits_ == other.digits_)
+        return *this;
+    if(digits_)
+        delete[] digits_;
+    quantity_ = other.quantity_;
+    digits_ = new int [quantity_];
+    for(int i = 0; i < quantity_; ++i)
+    {
+        digits_[i] = other.digits_[i];
+    }
+    return *this;
+}
+
+bool natural::operator==(const natural &other)
+{
+    if(quantity_ != other.quantity_)
+        return false;
+    for(int i = 0; i < quantity_; ++i)
+    {
+        if(digits_[i] != other.digits_[i])
+            return false;
+    }
+    return true;
+}
+
+void natural::print()
+{
+    for(int i = quantity_ - 1; i >= 0; --i){
+        std::cout << digits_[i];
+    }
+    std::cout << std::endl;
+}
--- ComputerAlgebraSystem/natural.h
+++ ComputerAlgebraSystem/natural.h
@@ -4,10 +4,16 @@
 
 class natural {
 public:
+    natural();
     natural(const std::string& str);
+    natural(const natural& other);
+    ~natural();
+    natural& operator= (const natural& other);
+    bool operator== (const natural& other);
+    void print();
 private:
     int quantity_;
-    int digits_;
+    int* digits_;
 };
 
 
