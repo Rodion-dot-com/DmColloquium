936f6d56f3e4fbd4111237ec411c007aed05f149
rodion <rad89289317404@mail.ru>
2020-04-09 20:24:43 +0400

    natural declaration

--- ComputerAlgebraSystem/natural.h
+++ ComputerAlgebraSystem/natural.h
@@ -7,10 +7,13 @@ public:
     natural();
     natural(const std::string& str);
     natural(const natural& other);
+    natural(const int quantity, const int* digits);
     ~natural();
-    natural& operator= (const natural& other);
-    bool operator== (const natural& other);
+    natural & operator=(const natural& other);
+    bool operator==(const natural& other);
     void print();
+    int get_quantity() const;
+    int* get_digits() const;
 private:
     int quantity_;
     int* digits_;
