bfcf91f8efc278c61a5d894830e7b5b908f4d365
rodion <rad89289317404@mail.ru>
2020-04-09 19:58:21 +0400

    integer declaration

--- ComputerAlgebraSystem/integer.h
+++ ComputerAlgebraSystem/integer.h
@@ -3,7 +3,22 @@
 
 
 class integer {
-
+public:
+    integer();
+    integer(const std::string& str);
+    integer(const integer& other);
+    integer(const int quantity, const int* digits, const bool sign);
+    integer(const natural& other);
+    integer & operator=(const integer& other);
+    ~integer();
+    void print();
+    int get_quantity() const;
+    int * get_digits() const;
+    bool get_sign() const;
+private:
+    int quantity_;
+    int* digits_;
+    bool sign_;
 };
 
 
